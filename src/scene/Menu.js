5efa1e73185cc4f29dcf1924a09d7291c6010f87
Felix Li <lbc1125wj@gmail.com>
2021-04-10 01:35:02 -0700

    Update Menu info & add rocket movement

--- src/scene/Menu.js
+++ src/scene/Menu.js
@@ -13,8 +13,13 @@ class Menu extends Phaser.Scene
 
     create()
     {
+        //Set Menu info text
         this.add.text(20, 20, "Rocket Patrol Menu").setOrigin(-1.25, -2);
-        this.add.text(50, 50, "Click 'Enter' to play!").setOrigin(-0.8, -20);
+        this.add.text(20, 20,  "'A' - Left Move     'D' - Right Move").setOrigin(-0.4, -15);
+        this.add.text(20, 20, "'K' - Shoot").setOrigin(-2.3, -18);
+        this.add.text(50, 50, "Click 'Enter' to play!").setOrigin(-0.8, -22);
+
+        //Handle Scene Transition
         this.input.keyboard.on('keydown-ENTER', this.BeginPlay, this);
     }
 }
--- src/scene/Play.js
+++ src/scene/Play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene
     preload()
     {
         this.load.image("starfield", "assets/starfield.png");
+        this.load.image("rocket", "assets/rocket.png");
     }
 
     create()
@@ -15,8 +16,13 @@ class Play extends Phaser.Scene
             0, 0, 640, 480, "starfield"
         ).setOrigin(0,0);
 
+        this.rocket = this.add.sprite(
+            game.config.width / 2, game.config.height - 45, "rocket"
+        ).setOrigin(0,0);
+        
+
         //green UI
-        this.add.rectangle(0, borderPadding, 
+        this.add.rectangle(0, borderPadding + borderUIsize, 
                            game.config.width, borderUIsize * 2, 
                            0x00FF00).setOrigin(0,0);
 
@@ -25,12 +31,23 @@ class Play extends Phaser.Scene
 	    this.add.rectangle(0, game.config.height - borderUIsize, game.config.width, borderUIsize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUIsize, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-
         
+        //detect keyboard input
+        this.leftMove = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.rightMove = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update() 
     {
-        this.starfield.tilePositionX -= 1;
+        this.speed = 1;
+        if(this.leftMove.isDown && this.rocket.x > borderUIsize)
+        {
+            this.rocket.x -= this.speed;
+        }
+        if(this.rightMove.isDown && this.rocket.x < game.config.width - borderUIsize * 1.5)
+        {
+            this.rocket.x += this.speed;
+        }
+        this.starfield.tilePositionX -= 2;
     }
 }
