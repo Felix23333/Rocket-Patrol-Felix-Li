96863404caaff11e1a57c85ff48538995d63d249
Felix Li <lbc1125wj@gmail.com>
2021-04-09 20:34:51 -0700

    Add Scene Switch

--- src/scene/Menu.js
+++ src/scene/Menu.js
@@ -1,13 +1,20 @@
+
 class Menu extends Phaser.Scene
 {
     constructor()
     {
         super("menuScene");
     }
+    
+    BeginPlay()
+    {
+        this.scene.start("playScene");
+    }
 
     create()
     {
-        this.add.text(20, 20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+        this.add.text(20, 20, "Rocket Patrol Menu").setOrigin(-1.25, -2);
+        this.add.text(50, 50, "Click 'Enter' to play!").setOrigin(-0.8, -20);
+        this.input.keyboard.on('keydown-ENTER', this.BeginPlay, this);
     }
 }
